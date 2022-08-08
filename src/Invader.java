563f7ea430743ea2322b21d42d7bb071a4904c75
Shahil Patel <92860036+Shahil-Patel-3602@users.noreply.github.com>
2022-08-08 11:06:38 +0530

    Invader file edited to set vertical, horizontal positions, as well as state of success, caught during the process.

--- src/Invader.java
+++ src/Invader.java
@@ -1,8 +1,22 @@
 //importing java library for generating random numbers
 
+import javax.xml.stream.FactoryConfigurationError;
 import java.util.Random;
 
 public class Invader {
     //these class file has details regarding movements of Invader (Infiltrator)
+    int length;   //determine the horizontal position of the Invader
+    int width;    //determine the vertical position of the Invader
+    Boundary type;
+    boolean success; //variable to check whether the invader has crossed the fence or not.
+    boolean caught; //variable to check if Invader was caught by sensors.
+
+    public Invader() {   //initializing the variables for object Invader
+        this.success = false;
+        this.caught = false;
+        this.length = 0;
+        this.width = 0;
+        this.type = new Boundary(width, length);
+    }
 
 }
