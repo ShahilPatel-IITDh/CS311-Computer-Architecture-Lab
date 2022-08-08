e371f744ca0474c6dd0cab775260382f36d6db01
Shahil Patel <83348636+shahilpatel-3602@users.noreply.github.com>
2022-08-08 23:07:40 +0530

    Working code for getting time, need to change method of calculation to current_time and durations.

--- Assignment0/src/Time.java
+++ Assignment0/src/Time.java
@@ -1,5 +1,36 @@
+//These class file calculates the time taken to cross the boundary.
+
+import java.time.LocalTime;
+//these package will be used to generate the local time.
+import java.time.Duration;
+
 public class Time {
     private int time;
+    private LocalTime initial_time;
+    private LocalTime current_time;
+    int iteration = 0;
+    Time(){
+        initial_time = LocalTime.now(); //initialize the initial_time with the current time i.e. time while executing the file.
+        iteration++;
+    }
+
+    public LocalTime getinitial_time(){
+        return initial_time;
+    }
+    public LocalTime getcurrent_time(){
+        return current_time;
+    }
+
+    public void setcurrent_time(){
+        this.current_time = LocalTime.now();
+    }
+
+    public int getduration(){
+        Duration duration = Duration.between(initial_time, current_time); //the duration package is used here to get time difference between the initial and current time.
+        
+        return (int) duration.getSeconds(); //return seconds of time difference between the initial time and current time in integer format.
+    }
+
 
     //method to set time using 'this'
     public Time(int t){
