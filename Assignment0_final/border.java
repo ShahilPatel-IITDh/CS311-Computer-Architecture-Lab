a9c23e92b03d6dd05c68b7a643de7e88f186c6ed
Jade-26 <93079158+Jade-26@users.noreply.github.com>
2022-08-09 15:50:11 +0530

    Add files via upload

---
+++ Assignment0_final/border.java
@@ -0,0 +1,11 @@
+public class border {
+    int [][] border;
+    
+    public border(){
+        border = new int[3][3];
+    }
+    public void main(String args[])
+    {
+        // return bord;
+    }
+}
