75cfb15f568d3deacd38c05f0c55cd3de00cbce6
Shahil Patel <83348636+shahilpatel-3602@users.noreply.github.com>
2022-08-18 23:24:18 +0530

    Rajat's even-odd

--- Assignment-1/Files/even-odd.asm
+++ Assignment-1/Files/even-odd.asm
@@ -1,14 +1,20 @@
-    .data
+	.data
 a:
-	101
-	.text
+	10 
+    .text
 main:
-	load %x0, $a, %x4
-	divi %x4, 2, %x6
-	muli %x6, 2, %x6
-	beq %x4, %x6, even
-	addi %x0, 1, %x10
-	end
-even:
-	subi %x0, 1, %x10
-	end
+    add %x0, %x0, %x10
+    load %x0, $a, %x4
+    addi %x10, 2, %x5
+    subi %x5, 1, %x6
+    subi %x6, 1, %x7
+    div %x4, %x5, %x8
+    beq %x31, %x6, Odd
+    beq %x31, %x7, Even
+    end
+Odd:
+    add %x31, %x0, %x10
+    end
+Even:
+    subi %x31, 1, %x10
+    end
