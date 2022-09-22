cf95aa4ef369aba2f8ee59784932d3a1b3716132
Shahil Patel <83348636+shahilpatel-3602@users.noreply.github.com>
2022-09-22 22:51:40 +0000

    Simulator.java file's code completed

--- Assignment-3/Shahil/src/generic/Simulator.java
+++ Assignment-3/Shahil/src/generic/Simulator.java
@@ -2,7 +2,16 @@ package generic;
 
 import processor.Clock;
 import processor.Processor;
+import java.nio.ByteBuffer;
 
+
+import java.io.IOException;
+import java.io.InputStream;
+import processor.memorysystem.MainMemory;
+
+import java.io.FileInputStream;
+
+import processor.pipeline.RegisterFile;
 public class Simulator {
 		
 	static Processor processor;
@@ -18,16 +27,58 @@ public class Simulator {
 	
 	static void loadProgram(String assemblyProgramFile)
 	{
-		/*
-		 * TODO
-		 * 1. load the program into memory according to the program layout described
-		 *    in the ISA specification
-		 * 2. set PC to the address of the first instruction in the main
-		 * 3. set the following registers:
-		 *     x0 = 0
-		 *     x1 = 65535
-		 *     x2 = 65535
-		 */
+	
+		//  * TODO
+		//  * 1. load the program into memory according to the program layout described
+		//  *    in the ISA specification
+		 
+
+		 try(
+			InputStream inputStream = new FileInputStream(assemblyProgramFile);
+		 ){
+			MainMemory mainMemory = new MainMemory();
+			RegisterFile register = new RegisterFile();
+
+			byte[] arr = new byte[4];
+			int i=0, j=0, pc=0;
+
+			while(inputStream.read(arr) != -1){
+
+				ByteBuffer w = ByteBuffer.wrap(arr);
+
+				int n= w.getInt();
+
+				if(j==0){
+					pc = n;
+				}
+
+				if(j!=0){
+					mainMemory.setWord(i,n);
+					i+=1;
+				}
+
+				j+=1;
+			}
+
+			processor.setMainMemory(mainMemory);
+
+			// * 2. set PC to the address of the first instruction in the main
+			register.setProgramCounter(pc);
+
+			// 3. set the following registers:
+			//     x0 = 0
+			//     x1 = 65535
+			//     x2 = 65535
+
+			register.setValue(0,0);
+			register.setValue(1,65535);
+			register.setValue(2,65535);
+
+			processor.setRegisterFile(register);
+
+			}catch (IOException ex) {
+			ex.printStackTrace();
+		}
 	}
 	
 	public static void simulate()
