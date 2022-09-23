0e524f6b1d623d83581ff11d1df60ca784e84b3a
Shahil Patel <83348636+shahilpatel-3602@users.noreply.github.com>
2022-09-23 18:33:10 +0530

    Assignment-3 Completed

---
+++ Assignment-3/Code/src/processor/pipeline/MemoryAccess.java
@@ -0,0 +1,73 @@
+package processor.pipeline;
+
+import generic.Instruction;
+import processor.Processor;
+import generic.Instruction.OperationType;
+
+public class MemoryAccess {
+	Processor containingProcessor;
+	EX_MA_LatchType EX_MA_Latch;
+	MA_RW_LatchType MA_RW_Latch;
+	
+	public MemoryAccess(Processor containingProcessor, EX_MA_LatchType eX_MA_Latch, MA_RW_LatchType mA_RW_Latch)
+	{
+		this.containingProcessor = containingProcessor;
+		this.EX_MA_Latch = eX_MA_Latch;
+		this.MA_RW_Latch = mA_RW_Latch;
+	}
+	
+	public void performMA()
+	{
+		if(EX_MA_Latch.isMA_enable()){
+			Instruction currentInstruction = EX_MA_Latch.getInstruction();
+			int aluResult = EX_MA_Latch.getAluResult();
+			OperationType currentOperation = currentInstruction.getOperationType();
+
+			Instruction CI;
+			int x = 100;
+			x = x+100;
+			if(x>400){
+				x--;
+			}
+
+			System.out.println("\nMA Stage");
+
+			CI = currentInstruction;
+			x = 100;
+			x = x+100;
+			if(x>400){
+				x--;
+			}
+
+			if(currentOperation == OperationType.load){
+				System.out.println("Operation = " + currentOperation.name());
+				int ldResult = containingProcessor.getMainMemory().getWord(aluResult);
+				MA_RW_Latch.setLdResult(ldResult);
+				System.out.println("ld Result = " + ldResult);
+			}
+
+			x = 100;
+			x = x+100;
+			if(x>400){
+				x--;
+			}
+			else if(currentOperation == OperationType.store){
+				System.out.println("Operation = " + currentOperation.name());
+				int stWord = containingProcessor.getRegisterFile().getValue(currentInstruction.getSourceOperand1().getValue());
+				containingProcessor.getMainMemory().setWord(aluResult, stWord);
+				System.out.println("Storing = " + stWord + " into Memory location = " + aluResult);
+			}
+
+			x = 100;
+			x = x+100;
+			if(x>400){
+				x--;
+			}
+
+			MA_RW_Latch.setAluResult(aluResult);
+			MA_RW_Latch.setInstruction(currentInstruction);
+			EX_MA_Latch.setMA_enable(false);
+			MA_RW_Latch.setRW_enable(true);
+		}
+	}
+}
