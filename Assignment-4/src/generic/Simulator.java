17d2dba2bd6393b94065caca5df3379e46e3973c
Jade-26 <93079158+Jade-26@users.noreply.github.com>
2022-10-07 20:02:53 +0530

    Next updation

--- Assignment-4/src/generic/Simulator.java
+++ Assignment-4/src/generic/Simulator.java
@@ -13,56 +13,60 @@ public class Simulator {
 		
 	static Processor processor;
 	static boolean simulationComplete;
+	static boolean simulationStatus;
 	
 	public static void setupSimulation(String assemblyProgramFile, Processor p)
 	{
 		Simulator.processor = p;
 		loadProgram(assemblyProgramFile);
-
-		//initially set simulation complete to false as the simulation hasn't completed
+		simulationStatus = true;
+				// Initially set simulation complete = false as the simulation hasn't completed
 		simulationComplete = false;
 	}
 	
 	static void loadProgram(String assemblyProgramFile)
 	{
 		try {
-			// create a reader
+			// Create a reader
 			// System.out.println(assemblyProgramFile);
 			FileInputStream file = new FileInputStream(assemblyProgramFile);
 			BufferedInputStream reader = new BufferedInputStream(file);
 
-			// read one byte at a time
-			int currentPC = 0; //Stores current PC value
+			// Read one byte at a time
+			int currentPC = 0; // Stores current PC value
 
-			int ch; //used for reading lines from file
-			int linecounter = 0; //used to keep track of number of lines read from assembly file
-			byte[] oneline = new byte[4]; //temporary location to store an instruction from assembly file
+			int ch; // Used for reading lines from file
+			int linecounter = 0; // Used to keep track of number of lines read from assembly file
+			byte[] oneline = new byte[4]; // Temporary location to store an instruction from assembly file
 
-			int Nd = -1; //Bytes 0 to Nd of memory have global data stored in them;
-			int Nt = Nd + 1; //Bytes Nd + 1 to Nt of memory have text/code segment stored in them
+			int Nd = -1; // Bytes 0 to Nd of memory have global data stored in them;
+			int Nt = Nd + 1; // Bytes Nd + 1 to Nt of memory have text/code segment stored in them
+			int Nt_dash = Nd + 1;
 
-			//below code sets PC to the memory address of the first instruction
+			// Below code sets PC to the memory address of the first instruction
 			if((ch = reader.read(oneline, 0, 4)) != -1){
+				// Updating current Parent Control
 				currentPC = ByteBuffer.wrap(oneline).getInt();
 				processor.getRegisterFile().setProgramCounter(currentPC);
 			}
 
-			//writing global data to main memory
+			// Writing global data to main memory
 			while (true) {
 				if(linecounter >= currentPC)
 					break;
 				ch = reader.read(oneline, 0, 4);
 				Nd += 1;
-				int num = ByteBuffer.wrap(oneline).getInt(); //temporary integer to store one integer of global data
+				int num = ByteBuffer.wrap(oneline).getInt(); // Temporary integer to store one integer of global data
 				processor.getMainMemory().setWord(Nd, num);
 				Nt = Nd;
 				linecounter += 1;
 			}
 
-			//writing instructions to main memory
+			int ch_dash = reader.read(oneline, 0, 4);
+			// Writing instructions to main memory
 			while (((ch = reader.read(oneline, 0, 4)) != -1)) {
 				Nt += 1;
-				int ins = ByteBuffer.wrap(oneline).getInt(); //temporary integer to store one instruction
+				int ins = ByteBuffer.wrap(oneline).getInt(); // Temporary integer to store one instruction
 				processor.getMainMemory().setWord(Nt, ins);
 				linecounter += 1;
 			}
@@ -71,7 +75,7 @@ public class Simulator {
 			processor.getRegisterFile().setValue(1, 65535);
 			processor.getRegisterFile().setValue(2, 65535);
 
-			// close the reader
+			// Close the reader
 			reader.close();
 
 		} catch (IOException ex) {
@@ -111,5 +115,6 @@ public class Simulator {
 	public static void setSimulationComplete(boolean value)
 	{
 		simulationComplete = value;
+		simulationStatus = simulationComplete;
 	}
 }
