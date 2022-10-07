2ff4f683408f151a76feefe9a01e6af4e092440e
Shahil Patel <83348636+shahilpatel-3602@users.noreply.github.com>
2022-10-07 19:50:02 +0530

    Working src

--- Assignment-4/src/generic/Simulator.java
+++ Assignment-4/src/generic/Simulator.java
@@ -13,60 +13,56 @@ public class Simulator {
 		
 	static Processor processor;
 	static boolean simulationComplete;
-	static boolean simulationStatus;
 	
 	public static void setupSimulation(String assemblyProgramFile, Processor p)
 	{
 		Simulator.processor = p;
 		loadProgram(assemblyProgramFile);
 
-		simulationstalled = true; 
-		simulationComplete = false; // Initially set simulation complete to false as the simulation hasn't completed
-		simulationStatus = !simulationComplete;
+		//initially set simulation complete to false as the simulation hasn't completed
+		simulationComplete = false;
 	}
 	
 	static void loadProgram(String assemblyProgramFile)
 	{
 		try {
-			// Create a reader
+			// create a reader
+			// System.out.println(assemblyProgramFile);
 			FileInputStream file = new FileInputStream(assemblyProgramFile);
 			BufferedInputStream reader = new BufferedInputStream(file);
 
-			// Read one byte at a time
-			int currentPC = 0; // Current PC value
+			// read one byte at a time
+			int currentPC = 0; //Stores current PC value
 
-			int ch; // Used for reading lines from file
-			int linecounter = 0; // Used to keep track of number of lines read from assembly file
-			byte[] oneline = new byte[4]; // Temporary location to store an instruction from assembly file
+			int ch; //used for reading lines from file
+			int linecounter = 0; //used to keep track of number of lines read from assembly file
+			byte[] oneline = new byte[4]; //temporary location to store an instruction from assembly file
 
-			int Nd = -1; // Bytes 0 to Nd of memory have global data stored in them;
-			int Nt = Nd + 1; // Bytes Nd + 1 to Nt of memory have text/code segment stored in them
+			int Nd = -1; //Bytes 0 to Nd of memory have global data stored in them;
+			int Nt = Nd + 1; //Bytes Nd + 1 to Nt of memory have text/code segment stored in them
 
-			// Below code sets PC to the memory address of the first instruction
-			int ch_dash = reader.read(oneline,0,4);
-
-			if((ch = ch_dash != -1){
+			//below code sets PC to the memory address of the first instruction
+			if((ch = reader.read(oneline, 0, 4)) != -1){
 				currentPC = ByteBuffer.wrap(oneline).getInt();
 				processor.getRegisterFile().setProgramCounter(currentPC);
 			}
-			boolean sett = false;
-			
-			// Writing global data to main memory
+
+			//writing global data to main memory
 			while (true) {
 				if(linecounter >= currentPC)
 					break;
 				ch = reader.read(oneline, 0, 4);
 				Nd += 1;
-				int num = ByteBuffer.wrap(oneline).getInt(); // Temporary integer to store one integer of global data
+				int num = ByteBuffer.wrap(oneline).getInt(); //temporary integer to store one integer of global data
 				processor.getMainMemory().setWord(Nd, num);
 				Nt = Nd;
 				linecounter += 1;
 			}
 
-			// Writing instructions to main memory
-			while (((ch = ch_dash != -1)) {
+			//writing instructions to main memory
+			while (((ch = reader.read(oneline, 0, 4)) != -1)) {
 				Nt += 1;
-				int ins = ByteBuffer.wrap(oneline).getInt(); // Temporary integer to store one instruction
+				int ins = ByteBuffer.wrap(oneline).getInt(); //temporary integer to store one instruction
 				processor.getMainMemory().setWord(Nt, ins);
 				linecounter += 1;
 			}
@@ -75,7 +71,7 @@ public class Simulator {
 			processor.getRegisterFile().setValue(1, 65535);
 			processor.getRegisterFile().setValue(2, 65535);
 
-			// Close the reader
+			// close the reader
 			reader.close();
 
 		} catch (IOException ex) {
@@ -88,22 +84,22 @@ public class Simulator {
 		Statistics.setNumberOfInstructions(0);
 		Statistics.setNumberOfCycles(0);
 
-		while(simulationStatus)
+		while(!simulationComplete)
 		{
-			// Executing instructions according to given in the question
-			// Perfrom RW
+			//executing instructions according to given in the question
+			//perfrom RW
 			processor.getRWUnit().performRW();
 			
-			// Perform MA
+			// perform MA
 			processor.getMAUnit().performMA();
 			
-			// Perform EX
+			//perform EX
 			processor.getEXUnit().performEX();
 			
-			// Perform OF
+			//perform OF
 			processor.getOFUnit().performOF();
 			
-			// Perform IF
+			//perform IF
 			processor.getIFUnit().performIF();
 			Clock.incrementClock();
 
@@ -116,7 +112,4 @@ public class Simulator {
 	{
 		simulationComplete = value;
 	}
-
-    public static void assemble(String string) {
-    }
 }
