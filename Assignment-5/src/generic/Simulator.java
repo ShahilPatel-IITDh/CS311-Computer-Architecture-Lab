23ee520c8dd0f36f26ca3cd21adf6880e3bbb422
Shahil Patel <83348636+shahilpatel-3602@users.noreply.github.com>
2022-10-19 21:22:59 +0530

    Final Zip file

--- Assignment-5/src/generic/Simulator.java
+++ Assignment-5/src/generic/Simulator.java
@@ -16,6 +16,7 @@ public class Simulator {
 	//As mentioned in PDF add EventQueue eventQueue
 	static EventQueue eventQueue;
 	public static int noOfInstructions;
+	static boolean Version_control = false; // setting a boolean counter //DC
 	
 	public static void setupSimulation(String assemblyProgramFile, Processor p)
 	{
@@ -29,11 +30,27 @@ public class Simulator {
 		simulationComplete = false;
 	}
 	
+	public static void Version_control_shift(boolean VC) //DC
+	{
+		if(Version_control == true)		
+			{
+			Version_control = true;
+			}
+		else
+		{
+			Version_control = false;
+		}
+	}
+	
 	static void loadProgram(String assemblyProgramFile)
 	{
 		try {
 			// create a reader
 			System.out.println(assemblyProgramFile);
+			if(Version_control == true)
+			{
+			Version_control = false;
+			}
 			FileInputStream file = new FileInputStream(assemblyProgramFile);
 			BufferedInputStream reader = new BufferedInputStream(file);
 
@@ -44,12 +61,21 @@ public class Simulator {
 			int linecounter = 0; //used to keep track of number of lines read from assembly file
 			byte[] oneline = new byte[4]; //temporary location to store an instruction from assembly file
 
+			if(Version_control == true)
+			{
+			Version_control = false;
+			}
+
 			int Nd = -1; //Bytes 0 to Nd of memory have global data stored in them;
 			int Nt = Nd + 1; //Bytes Nd + 1 to Nt of memory have text/code segment stored in them
 
 			//below code sets PC to the memory address of the first instruction
 			if((ch = reader.read(oneline, 0, 4)) != -1){
 				currentPC = ByteBuffer.wrap(oneline).getInt();
+				if(Version_control == true)		//DC
+				{
+				Version_control = false;
+				}
 				processor.getRegisterFile().setProgramCounter(currentPC);
 			}
 
@@ -61,6 +87,12 @@ public class Simulator {
 				ch = reader.read(oneline, 0, 4);
 				Nd += 1;
 				int num = ByteBuffer.wrap(oneline).getInt(); //temporary integer to store one integer of global data
+				//DC
+				if(Version_control == true)
+				{
+				Version_control = false;
+				}
+
 				processor.getMainMemory().setWord(Nd, num);
 				Nt = Nd;
 				linecounter += 1;
@@ -70,12 +102,22 @@ public class Simulator {
 			while (((ch = reader.read(oneline, 0, 4)) != -1)) {
 				Nt += 1;
 				int ins = ByteBuffer.wrap(oneline).getInt(); //temporary integer to store one instruction
+				//DC
+				if(Version_control == true)
+				{
+				Version_control = false;
+				}
 				processor.getMainMemory().setWord(Nt, ins);
 				linecounter += 1;
 			}
 
 			processor.getRegisterFile().setValue(0, 0);
 			processor.getRegisterFile().setValue(1, 65535);
+			//DC
+			if(Version_control == true)
+			{
+			Version_control = false;
+			}
 			processor.getRegisterFile().setValue(2, 65535);
 
 			// close the reader
@@ -98,6 +140,11 @@ public class Simulator {
 	public static void simulate()
 	{
 		Statistics.setNumberOfInstructions(0);
+		//DC
+		if(Version_control == true)
+			{
+			Version_control = false;
+			}
 		Statistics.setNumberOfCycles(0);
 		int noOfCycles = 0;
 
@@ -109,6 +156,11 @@ public class Simulator {
 
 			//added as mentioned in PDF
 			eventQueue.processEvents();
+			//DC
+			if(Version_control == true)
+			{
+			Version_control = false;
+			}
 
 			processor.getOFUnit().performOF();
 			processor.getIFUnit().performIF();
@@ -119,6 +171,11 @@ public class Simulator {
 		}
 		Statistics.setNumberOfInstructions(noOfInstructions);
 		Statistics.setNumberOfCycles(noOfCycles);
+		//DC
+		if(Version_control == true)
+		{
+			Version_control = false;
+		}
 		Statistics.setIPC();
 	}
 	
